3bf5900d1bc88f31f508caf28fa4afc510660798
Felix Blanchette <1742659@cegeplimoilou.ca>
2024-02-09 16:27:27 -0500

    SWAYYYYYYYYYY

--- S_tp1/Program.cs
+++ S_tp1/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("When marimba rythm starts to play dance with me make me sway like a lazy ocean hugs the shore hold me close sway me more");
+            Console.WriteLine("Other dancers may be on the floor dear but my eyes will see only you only you have that magic technic when we sway I go weak");
         }
     }
 }
