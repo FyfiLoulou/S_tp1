ee15ef7f486ef7ddb545719e127777745ea0dec8
Louis-Charles Biron <louirlesbiron6@gmail.com>
2024-02-20 12:23:34 -0500

    modifications

--- S_tp1/Evaluation.cs
+++ S_tp1/Evaluation.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 
+namespace S_tp1 {
+    public class Evaluation {
 
-namespace S_tp1
-{
-    public class Evaluation
-    {
-        public Evaluation() {
-        
+        private string identifiantUtilisateur;
+        private string identifiantMedia;
+        private byte cote;
+
+
+        public Evaluation(string identifiantUtilisateur, string identifiantMedia, byte cote) {
+            this.identifiantUtilisateur = identifiantUtilisateur;
+            this.identifiantMedia = identifiantMedia;
+            this.cote = cote;
         }
+
+        public string getIdentifiantUtilisateur() { return identifiantUtilisateur; }
+        public string getIdentifiantMedia() {  return identifiantMedia; }
+        public byte getCote() {  return cote; }  
 
     }
 }
