d6a933660f0b542701260c002d4139c49bdfa45b
Alakazouu <2033482@cegeplimoilou.ca>
2024-02-20 11:30:12 -0500

    utilisateur

--- S_tp1/Utilisateur.cs
+++ S_tp1/Utilisateur.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using static S_tp1.Evaluation;
 namespace S_tp1
 {
     public class Utilisateur
     {
+        //attributs
+        private String identifiantUnique;
+        private static int nombreIncremente;
+        private String pseudo;
+        private String motDePasse;
+        private String nom;
+        private String prenom;
+        private Enum role;
+        private List<String> favoris;
+        private Dictionary<String, Evaluation> evaluations;
+        
+        //constructor
         public Utilisateur()
         {
 
