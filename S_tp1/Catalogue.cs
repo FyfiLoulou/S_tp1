42b46d44c976fb578681feb2984502c5d3199936
Felix Blanchette <1742659@cegeplimoilou.ca>
2024-02-20 11:54:46 -0500

    debut catalogue + hiérarchie dossiers

--- S_tp1/Catalogue.cs
+++ S_tp1/Catalogue.cs
@@ -1,9 +1,35 @@
-﻿using System;
-namespace S_tp1
+﻿namespace S_tp1
 {
+    /*
+     * Regroupe les médias et s'occupe de la sérialisation des objets
+     */
     public class Catalogue
     {
-        
+        // Constructeur par défaut
+        public Catalogue() { }
+
+        /*
+         * Permet d'ajouter un media 
+         */
+        public bool ajouter(string identifiantMedia)
+        {
+            return true;
+        }
+
+        public bool remplacer(string identifiantMediaToAdd, string identifiantMediaToRemove)
+        {
+            return true;
+        }
+
+        public bool supprimer(string identifiantMedia) { return true; }
+
+        public bool supprimer() { return true; }
+
+        public string sauvegarder(string nomFichierSauvegarde)
+        {
+            return "";
+        }
+
     }
 }
 
