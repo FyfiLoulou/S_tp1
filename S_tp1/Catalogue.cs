a42c6ba44b8875bbc21658c58cc3f7038335912d
Felix Blanchette <1742659@cegeplimoilou.ca>
2024-02-20 12:40:46 -0500

    catalogue.cs on the way to the moon

--- S_tp1/Catalogue.cs
+++ S_tp1/Catalogue.cs
@@ -1,31 +1,72 @@
 ﻿namespace S_tp1
 {
     /*
-     * Regroupe les médias et s'occupe de la sérialisation des objets
+     * Regroupe les médias et s'occupe de la sérialisation de l'objet catalogue
      */
     public class Catalogue
     {
+        //TODO: 20-02-2024 -> définir le path du fichier de sauvegarde
+        private const string PATH_FICHIER_SAUVEGARDE = "path du fichier";
+
+        //TODO: 20-02-2024 -> maybe singleton, getInstance et le constructeur private maybe
+        private static List<Media>? catalogue;
+
+
         // Constructeur par défaut
-        public Catalogue() { }
+        public Catalogue() 
+        {
+            catalogue = new List<Media>();
+        }
 
         /*
-         * Permet d'ajouter un media 
+         * ajoute un media passé en paramètre dans le catalogue
+         * 
+         * @param identifiantMedia -> l'identifiant unique du media à ajouter
+         * @return -> retourne vrai si le media a bel et bien été ajouter au catalogue
          */
-        public bool ajouter(string identifiantMedia)
+        public bool Ajouter(string identifiantMedia)
         {
             return true;
         }
 
-        public bool remplacer(string identifiantMediaToAdd, string identifiantMediaToRemove)
+        /*
+         * remplace un media passé en paramètre par un autre aussi passé en paramètre
+         * 
+         * @param identifiantMediaToAdd -> l'identifiant unique du media à ajouter
+         * @param identifiantMediaToRemove -> l'identifiant unique du media à remplacer
+         * @return -> retourne vrai si le media a bel et bien été remplacé
+         */
+        public bool Remplacer(string identifiantMediaToAdd, string identifiantMediaToRemove)
         {
             return true;
         }
 
-        public bool supprimer(string identifiantMedia) { return true; }
+        /*
+         * supprime le media passé en paramètre du catalogue
+         * 
+         * @param identifiantMedia -> l'identifiant unique du media à supprimer
+         * @return -> retourne vrai si le media a bel et bien supprimé
+         */
+        public bool Supprimer(string identifiantMedia) { return true; }
 
-        public bool supprimer() { return true; }
+        /*
+         * supprime le catalogue au complet
+         * 
+         * @return -> retourne vrai si le catalogue est supprimé correctement
+         */
+        public bool Supprimer() { return true; }
 
-        public string sauvegarder(string nomFichierSauvegarde)
+        /*
+         * sauvegarde le catalogue et le sérialise dans un fichier JSON
+         * 
+         * @param nomFichierSauvegarde -> le nom du fichier JSON de sauvegarde
+         */
+        public void Sauvegarder(string nomFichierSauvegarde)
+        {
+            
+        }
+        
+        public override string ToString()
         {
             return "";
         }
