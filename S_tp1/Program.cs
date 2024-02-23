cf3f3a3431bdf83aa9c75c8602f91615fb12b2db
Maek Lorman <2033482@cegeplimoilou.ca>
2024-02-23 13:30:09 -0500

    commencer getters utilisateurs  + tester si utilisateur marche

--- S_tp1/Program.cs
+++ S_tp1/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TP1 d'application web");
+            Console.WriteLine("TP1 d'application web\n");
+            /*
 
             Catalogue catalogue = new Catalogue();
 
@@ -17,7 +18,19 @@
             Console.WriteLine(catalogue.getCatalogue().Count);
 
             catalogue.Sauvegarder();
+            */
 
+            Utilisateur johnyTest = new Utilisateur("JohnyX", "abc123", "Test", "Johny");
+            Utilisateur bob = new Utilisateur("bob", "bob", "bob", "bob");
+            Utilisateur singe = new Utilisateur("singe", "singe", "singe", "singe");
+            Utilisateur tonTourFelix = new Utilisateur("tonTourFelix", "tonTourFelix", "tonTourFelix", "tonTourFelix");
+            Utilisateur hugo = new Utilisateur("hugo", "hugo", "hugo", "hugo");
+            Utilisateur hugo2 = new Utilisateur("hugo", "hugo", "hugo", "hugo");
+
+
+            
+            Console.WriteLine(hugo + "\n\n");
+            Console.WriteLine(hugo2 + "\n\n");
 
 
         }
