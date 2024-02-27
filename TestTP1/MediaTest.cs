09db7eb40b5c33c5d44e9280eb24e54526aaf2c7
Louis-Charles Biron <louirlesbiron6@gmail.com>
2024-02-27 12:47:46 -0500

    q

--- TestTP1/MediaTest.cs
+++ TestTP1/MediaTest.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using S_tp1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static S_tp1.Types;
 
 namespace TestTP1
 {
-    internal class MediaTest
+    public class MediaTest
     {
+
+        Catalogue catalogue;
+
+        public void Setup()
+        {
+            catalogue = new Catalogue();
+            mediaTest = new Media("media", Types.ROCK, 1, 1, "felix", "maek", "extrait", "complet", "image");
+        }
+
+        public void egalite() {
+            
+        }
+
     }
 }
