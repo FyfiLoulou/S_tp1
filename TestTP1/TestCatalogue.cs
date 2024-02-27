e5ec59f6c24aa35e3a290cf049e521b518bb3f4c
Felix Blanchette <1742659@cegeplimoilou.ca>
2024-02-27 13:03:22 -0500

    Squelette de test de catalogue

--- TestTP1/TestCatalogue.cs
+++ TestTP1/TestCatalogue.cs
@@ -4,6 +4,91 @@ namespace TestTP1
 {
     public class TestCatalogue
     {
+        Catalogue catalogue;
 
+
+        [SetUp]
+        public void Setup()
+        {
+            catalogue = new Catalogue();
+        }
+
+        [Test]
+        public void testConstructeur()
+        {
+            //Acteurs
+
+            //Actions
+
+            //Assertions
+        }
+
+        [Test]
+        public void etantDonneAjouterUnMedia_quandAjouterUnMedia_alorsUnMediaDansCatalogue()
+        {
+            //Acteurs
+
+            //Actions
+
+            //Assertions
+        }
+
+        [Test]
+        public void etantDonneAjouterUnFichier_quandAjouterUnFichier_alorsCatalogueRempliDeMedia()
+        {
+            //Acteurs
+
+            //Actions
+
+            //Assertions
+        }
+
+        [Test]
+        public void testSauvegarder()
+        {
+            //Acteurs
+
+            //Actions
+
+            //Assertions
+        }
+
+        [Test]
+        public void testSupprimerUnFichier()
+        {
+            //Acteurs
+
+            //Actions
+
+            //Assertions
+        }
+
+        [Test]
+        public void testSupprimerUnMedia()
+        {
+            //Acteurs
+
+            //Actions
+
+            //Assertions
+        }
+
+        public void TestRemplacer()
+        {
+            //Acteurs
+
+            //Actions
+
+            //Assertions
+        }
+
+        public void TestToString()
+        {
+            //Acteurs
+
+            //Actions
+
+            //Assertions
+        }
     }
 }
