28495d316123b392f903d858dfb93f9fa25d517b
felix.blanchette@gmail.com <felix.blanchette@gmail.com>
2024-03-19 10:44:19 -0400

    Propriétés utilisés

--- S_tp1/Evaluation.cs
+++ S_tp1/Evaluation.cs
@@ -9,27 +9,27 @@ namespace S_tp1 {
 
 
         public Evaluation(Utilisateur utilisateur, Media media, byte cote) {
-            this.utilisateur = utilisateur;
-            this.media = media;
+            Utilisateur = utilisateur;
+            Media = media;
             Cote = cote;
         }
 
         public Utilisateur Utilisateur
         {
             get { return utilisateur; }
-            set {}
+            set {utilisateur = value;}
         }
 
         public Media Media
         {
             get { return media; }
-            set {}
+            set {media = value;}
         }
 
         public byte Cote
         {
             get { return cote; }
-            set { this.cote = cote > 100 ? (byte)100 : cote; }
+            set { cote = value > 100 ? (byte)100 : value; }
         }
 
         public override string ToString() {
